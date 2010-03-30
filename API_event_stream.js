8986a08040b3ff23f242426a10c864243b00197a
inimino <inimino@inimino.org>
2010-03-31 01:30:09 +0200

    PEG: finished the streaming-event-handling API

--- API_event_stream.js
+++ API_event_stream.js
@@ -1,31 +1,34 @@
-function streamHandler(dict,names){var p,open_cb=[],close_cb=[],index,anon,fail,pos=0,pos_stack=[],rule_stack=[],name,open
+function nodeHandler(dict,names){var p,any,anon,fail,index,cb={},stack=[],frame,pos=0
  for(p in dict) if(dict.hasOwnProperty(p)){
-  if(p=='anonymous'){anon=dict[p];continue}
+  if(p=='any'){any=dict[p];continue}
+  if(p=='anon'){anon=dict[p];continue}
   if(p=='fail'){fail=dict[p];continue}
-  if(p.slice(-5)=='_open'){name=p.slice(0,-5);open=true}
-  else if(p.slice(-6)=='_close'){name=p.slice(0,-6);open=false}
-  else err('Unknown callback type: '+p+' (allowed callbacks include *_open, *_close, "anonymous", or "fail")')
-  index=names.indexOf(name)
+  index=names.indexOf(p)
   if(index==-1) err('rule not found in rule names: '+name)
-  ;(open?open_cb:close_cb)[index]=dict[p]}
- return function(events){var i,l,x
+  cb[index]=dict[p]}
+ return function(events,str){var i,l,x,retval
   for(i=0,l=events.length;i<l;i++){x=events[i]
-   if(x>0){ // named rule open
-    if(open_cb[x])open_cb[x](pos)
-    pos_stack.push(pos)
-    rule_stack.push(x)}
+   if(x>0){ // named rule start
+    stack.push(frame)
+    frame={index:x,start:pos}
+    if(cb[x]) frame.cn=[]}
    else if(x==-1){ // anonymous node
     i++
     if(i==l)err('incomplete anonymous node')
-    if(anon)anon(pos,pos+events[i])
+    if(anon)anon({start:pos,end:pos+events[i]})
     pos+=events[i]}
-   else if(x==-2){ // named_rule_close
+   else if(x==-2){ // node close
     i++
     if(i==l)err('incomplete rule close')
-    pos=pos_stack.pop() // pop the start position
-    x=rule_stack.pop() // pop the rule which is being closed
-    if(close_cb[x])close_cb[x](pos,pos+events[i])
-    pos+=events[i]}
-   else err('invalid event stream: saw '+x+' at position '+i)}}
+    pos=frame.start+events[i]
+    x=frame.index
+    if(cb[x]) retval=cb[x](m(frame.start,pos),frame.cn)
+    frame=stack.pop() // the parent node
+    if(retval!==undefined) frame.cn.push(retval)}
+   else err('invalid event stream: saw '+x+' at position '+i)}
+  function m(s,e){
+   return {start:s
+          ,end:e
+          ,text:function(){return str.slice(s,e)}}}}
  function err(s){
   throw new Error('streamHandler: '+s)}}
