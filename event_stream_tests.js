e8be9eddd64e640d63c9bf88b5efbda83ab227a6
inimino <inimino@inimino.org>
2010-03-31 21:39:05 +0200

    PanPG: the PEG project is renamed PanPG

--- event_stream_tests.js
+++ event_stream_tests.js
@@ -1,15 +1,16 @@
-function test_nodeHandler(){var dict,out=[],res,names,parser,s
+function test_nodeHandler(){var dict,out=[],result,names,parser,s
  s="1+2*3"
  out.push(s+'\n\n')
  parser=p_arith_streaming_v6_default_flags_Expr
  names=parser.names
- res=parser(s)
- assert(res[0],'parse succeeded')
+ result=parser(s)
+ assert(result[0],'parse succeeded')
+return showResult(result,names,s)
  dict=
   {Expr:function(_,cn){out.push('result: '+cn[0])}
   ,Add:function(_,cn){return sum(cn)}
   ,Mult:function(_,cn){return product(cn)}
   ,Num:function(m){return parseInt(m.text(),10)}
   }
- nodeHandler(dict,names)(res,s)
+ nodeHandler(dict,names)(result,s)
  return out.join('')}
