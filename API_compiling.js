bb6314512dab3f23a9cfd384627f0cce23ea96f5
inimino <inimino@inimino.org>
2010-04-05 21:47:20 +0200

    PanPG: removed the v6 codegen TAL dependency, ES5 parser now generated > 10x faster (98s to 9s).

--- API_compiling.js
+++ API_compiling.js
@@ -1,12 +1,22 @@
 function generateParser(peg,opts){var pt
- pt=parsePEG(peg)
+ pt=_parsePEG(peg)
  if(!pt[0])return pt
+ //return showTree(pt[1],parsePEG.names)
  pt=pt[1]
  PEG_codegen_5(pt)
  opts=opts||{}
  opts.codegen=opts.codegen||"v6"
  try{return [true,pt.code_v5()(opts)]}
  catch(e){return [false,e.toString()]}}
+
+function generateParser2(peg,opts){var pt,named_res
+ pt=parsePEG(peg)
+ if(!pt[0])return pt
+ //return parsePEG.names
+ //return showTree(pt[1],parsePEG.names)
+ named_res=v6_named_res(pt,parsePEG.names,peg)
+ //return pp(named_res)
+ return codegen_v6(opts||{},named_res)}
 
 function generateParserThrowing(peg,opts){var x
  x=generateParser(peg,opts)
@@ -15,7 +25,7 @@ function generateParserThrowing(peg,opts){var x
 
 // lower-level functions
 
-function parsePEG(peg){var pt
+function _parsePEG(peg){var pt
  peg=peg.replace(/^[\n\r]+|[\n\r]+$/g,'') // TODO: regenerate the PEG parser and fix this
  pt=p_PEG_v5_RuleSet(peg)
  if(!pt[0])return [false,showError(pt[1][0].RuleSet[1],"Parsing PEG input failed",peg)]
--- API_event_stream.js
+++ API_event_stream.js
@@ -1,10 +1,10 @@
-function nodeHandler(dict,names){var p,any,anon,fail,index,cb={},stack=[],frame,pos=0
+function walkTree(dict,names){var p,any,anon,fail,index,cb={},stack=[],frame,pos=0,warnings=[]
  for(p in dict) if(dict.hasOwnProperty(p)){
   if(p=='any'){any=dict[p];continue}
   if(p=='anon'){anon=dict[p];continue}
   if(p=='fail'){fail=dict[p];continue}
   index=names.indexOf(p)
-  if(index==-1) err('rule not found in rule names: '+name)
+  if(index==-1) err('rule not found in rule names: '+p)
   cb[index]=dict[p]}
  return function(result,str){var i,l,x,retval,events
   if(!result[0]){
@@ -26,10 +26,15 @@ function nodeHandler(dict,names){var p,any,anon,fail,index,cb={},stack=[],frame,
     if(i==l)err('incomplete rule close')
     pos=frame.start+events[i]
     x=frame.index
-    if(cb[x]) retval=cb[x](m(frame.start,pos),frame.cn)
+    if(cb[x])
+     try{retval=cb[x](m(frame.start,pos),frame.cn)}
+     catch(e){err('exception in '+names[x]+': '+e.toString())}
     frame=stack.pop() // the parent node
-    if(retval!==undefined) frame.cn.push(retval)}
+    if(cb[x] && retval!==undefined)
+     if(frame.cn)frame.cn.push(retval)
+     else warnings.push('ignored return value of '+names[x]+' in '+names[frame.index])}
    else err('invalid event stream: saw '+x+' at position '+i)}
+  return warnings
   function m(s,e){
    return {start:s
           ,end:e
