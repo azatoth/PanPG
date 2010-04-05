42cbffbddb82d372e754fe73dfb774284de62507
inimino <inimino@inimino.org>
2010-04-05 23:23:40 +0200

    PanPG: updated build, PEG_generator.js is now 79231 bytes, down from 123374 as of v0.0.5

--- API_compiling.js
+++ API_compiling.js
@@ -1,34 +1,13 @@
-function generateParser(peg,opts){var pt
- pt=_parsePEG(peg)
- if(!pt[0])return pt
- //return showTree(pt[1],parsePEG.names)
- pt=pt[1]
- PEG_codegen_5(pt)
- opts=opts||{}
- opts.codegen=opts.codegen||"v6"
- try{return [true,pt.code_v5()(opts)]}
- catch(e){return [false,e.toString()]}}
-
-function generateParser2(peg,opts){var pt,named_res
+function generateParser(peg,opts){var pt,named_res
  pt=parsePEG(peg)
  if(!pt[0])return pt
- //return parsePEG.names
- //return showTree(pt[1],parsePEG.names)
  named_res=v6_named_res(pt,parsePEG.names,peg)
- //return pp(named_res)
- return codegen_v6(opts||{},named_res)}
+ try{return [1,codegen_v6(opts||{},named_res)]}
+ catch(e){return [0,e.toString()]}}
 
 function generateParserThrowing(peg,opts){var x
  x=generateParser(peg,opts)
  if(!x[0])throw new Error(x[1])
  return x[1]}
-
-// lower-level functions
-
-function _parsePEG(peg){var pt
- peg=peg.replace(/^[\n\r]+|[\n\r]+$/g,'') // TODO: regenerate the PEG parser and fix this
- pt=p_PEG_v5_RuleSet(peg)
- if(!pt[0])return [false,showError(pt[1][0].RuleSet[1],"Parsing PEG input failed",peg)]
- return pt}
 
 function patchPEG(original,patch){}
--- build/build.js
+++ build/build.js
@@ -1,18 +1,12 @@
-function build_PEG_compiler(API_compiling,PEG_codegen_5,PEG_codegen_5_js,PEG_codegen_6_js,TAL_match_js,PEG_v5,re_js,lists_js,cset_prod_js,assert_js){var tree,talProg
- tree=PTNodeTALDict.fnParse(PEG_codegen_5)
- talProg=PTNodeTALDict.fnCompile(tree,{functionName:'PEG_codegen_5'})
+function build_PEG_compiler(API_compiling,PEG_codegen_6_attr_js,PEG_codegen_6_js,parsePEG_js,re_js,lists_js,cset_prod_js,assert_js){
  return '/* PEG → JavaScript parser generator, with its dependencies.\n * See http://inimino.org/~inimino/blog/peg_v0.0.5\n * MIT Licensed\n */\n\n'
       + ';(function(exports){\n\n'
       + 'exports.generateParser=generateParser\n'
-     // + 'exports.generateParserThrowing=generateParserThrowing\n'
+      + 'exports.generateParserThrowing=generateParserThrowing\n'
       + '\n'
       + API_compiling
-      + '\n\n /* PEG_v5 */ \n\n'
-      + PEG_v5
-      + '\n\n/* generated code, do not edit */\n'
-      + talProg
-      + '\n\n/* TAL supporting code */\n\n'
-       + TAL_match_js
+      + '\n\n /* parsePEG.js */ \n\n'
+      + parsePEG_js
       + '\n\n/* re.js */\n\n'
       + re_js
       + '\n\n/* lists.js */\n\n'
@@ -21,12 +15,12 @@ function build_PEG_compiler(API_compiling,PEG_codegen_5,PEG_codegen_5_js,PEG_cod
       + assert_js
       + '\n\n/* CSET */\n\n'
       + cset_prod_js
-      + '\n\n/* PEG_codegen_5.js */ \n\n'
-      + PEG_codegen_5_js
+      + '\nCSET.import(\'\',CSET)'
+      + '\n\n/* PEG_codegen_6_attr.js */ \n\n'
+      + PEG_codegen_6_attr_js
       + '\n\n/* PEG_codegen_6.js */ \n\n'
       + PEG_codegen_6_js
-      // + '\n\n/* PTNode.js (for showTree() only) */\n\n'
-      // + PTNode_js
       + '\n})(typeof exports==\'undefined\'?this:exports);\n'}
 
-// remaining dependencies:
+/* remaining dependencies:
+ */
