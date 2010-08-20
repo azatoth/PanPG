5c8629b2c26f5dc583bca87a1b2720ad1ad2eb88
inimino <inimino@inimino.org>
2010-08-20 05:26:39 +0200

    PanPG: "rule required but not defined" exception is back (instead of a less helpful exception)

--- src/codegen_tests.js
+++ src/codegen_tests.js
@@ -2,6 +2,9 @@ function buildES5_commonjs(es5,stream,opts){
  opts.patches=[stream]
  return generateParser(es5,opts)}
 
+function buildJSParser(es5,streamable,web_compat,opts){
+ return generateParser([es5,streamable,web_compat],opts)}
+
 function test_drop(peg,input){var opts
  opts={drop:['JSON'],debug:true}
  //return generateParser(peg,opts)
@@ -610,32 +613,6 @@ function peg_benchmarks_dfa_cset_maps(){var ret=[],ms,s,x,slen
  go('crazy switch',peg_benchmarks_map_switch)
  go('ridiculous array',peg_benchmarks_map_array_huge())
  return ret.join('\n')}
-
-////////////////////////////
-
-function benchmark(f,ms){var d1=+new Date,d2,end=d1+ms,res=[]
- do{
-  f()
-  d2=+new Date
-  res.push(d2-d1)
-  d1=d2}
- while(d2<end)
- return res}
-
-// function, number or ms to run, description, operations per call to f() (for scaling results comparably)
-function simple(f,ms,desc,ops){var res,n,tot_ms,most
- res=benchmark(f,ms)
- n=res.length
- tot_ms=sum(res)
- //most=res.sort(function(a,b){return a==b?0:a<b?-1:1}).slice(0,Math.round(res.length*0.5))
- return desc+':\n'+
-        ['ops/ms: '+(n*ops/tot_ms).toPrecision(4)
-        ,'ms: '+tot_ms
-        ,'max: '+max(res)
-        ,'min: '+min(res)
-        ,'n: '+n
-        ,'ops: '+ops
-        ].join(' ')}
 
 function peg_benchmarks_re_vs_loop(s){var t,i,l,re,re2,res=[],res2=[],s2,ret=[],x
  //s=Array(9).join(s)
