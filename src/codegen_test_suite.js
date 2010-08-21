2373d56ead0501e6edcb5ce5600a4fb25da093cd
inimino <inimino@inimino.org>
2010-08-21 06:42:25 +0200

    PanPG: ES5 grammar now uses "Expression" not "Expr" throughout, some other small tweaks for parse tree clarity

--- src/codegen_test_suite.js
+++ src/codegen_test_suite.js
@@ -1,18 +1,23 @@
+function codegen_test_all(){var n,i,results=[]
+ n=2
+ for(i=1;i<=n;i++){
+  results.push(i+' '+(eval('codegen_test_'+i)()?'PASS':'FAIL'))}
+ return results.join('\n')}
+
 var codegen_test_input_1=
 '["","a",null,true,false,0,-1,1,1.7,-1.7,1e3,-1e3,5.2e8,-5.2e8,{"a":"a","":"","true":true,"false":false,"null":null,"0":0},{},{"":{"":{}}},[],[[[]]]]'
 
-var codegen_test_grammar_1=
-'True ← "true"'
+var codegen_test_grammar_1='True ← "true"'
+  , codegen_test_grammar_2='S ← "a"'
 
-  , codegen_test_grammar_2=
-'S ← "a"'
-
-function codegen_test_001(){var code
- code=generateParser(codegen_test_grammar_1)
+function must_match(grammar,input){var code
+ code=generateParser(grammar,{fname:'__codegen_test'})
  eval(code)
- return True("true")[0]
- return explain(codegen_test_grammar_1,{},"true",true)}
+ return __codegen_test(input)[0]}
 
-function codegen_test_002(){
- return explain(codegen_test_grammar_2,{},'a')}
+function codegen_test_1(){
+ return must_match(codegen_test_grammar_1,"true")}
+
+function codegen_test_2(){
+ return must_match(codegen_test_grammar_2,'a')}
 
